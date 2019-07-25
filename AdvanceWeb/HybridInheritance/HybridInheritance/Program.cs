34e4f3cfa21f9b3b5b5fb120aaf429ca4c3454a5
Aastha Parmar <40237072+Animo-girl@users.noreply.github.com>
2019-07-25 13:32:32 +0530

    Update Program.cs

--- AdvanceWeb/HybridInheritance/HybridInheritance/Program.cs
+++ AdvanceWeb/HybridInheritance/HybridInheritance/Program.cs
@@ -15,6 +15,46 @@ namespace HybridInheritance
             dep_id = int.Parse(Console.ReadLine());
         }
     }
+    
+    class Employee
+    {
+        public void Displayname( string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        static void Main(string[] args)
+        {
+            Parttime p = new Parttime();
+            p.Displayname("AAstha");
+            p.Salary(5000);
+            FullTime f = new FullTime();
+            f.Displayname("Parmar");
+            f.Salary(20000);
+            Console.ReadKey();
+
+        }
+    }
+
+    class Parttime:Employee
+    {
+       public void Salary(int salary)
+        {
+            salary = salary * 2;
+            Console.WriteLine(salary);
+
+        }
+    }
+
+    class FullTime:Employee
+    {
+        public void Salary(int salary)
+        {
+            salary = salary * 2;
+            Console.WriteLine(salary);
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
