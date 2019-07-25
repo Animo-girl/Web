bf2530eafe447e5ffd0883c6b190333f7fe558a6
Atharva Pradhan <41517998+AtharvaPradhan@users.noreply.github.com>
2019-07-25 13:28:56 +0530

    Add files via upload

---
+++ AdvanceWeb/HyrarkiInheritance/HyrarkiInheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HyrarkiInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
